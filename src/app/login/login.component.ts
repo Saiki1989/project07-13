f20b599017b5da6c06e1ecc76ac4dd10b2cf8915
Tong <thongchaiwat589@gmail.com>
2024-09-10 12:25:06 +0700

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,62 +1,59 @@
-import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
 import { Component } from '@angular/core';
 import {
-  ReactiveFormsModule,
+  ReactiveFormsModule,  
   FormsModule,
-  FormGroup,
-  FormBuilder,
-  Validators,
-  EmailValidator
+  FormGroup,             
+  FormBuilder,           
+  Validators
 } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FormsModule], 
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']        
 })
 export class LoginComponent {
-  //FormGroup is here
-  loginForm!: FormGroup
-  //submit false/ture check
-  submitted = false;
-  //from conncect
-  userLogin = {
+  //form: FormGroup
+  loginFrom! : FormGroup;
+  //สร้างตัวแปรไว้เซ็ตว่า Submit from หรือยัง
+  submitted = false
+  //ตัวแปรสำหรับผูกกับฟอร์ม
+  userLogin ={
     "email": "",
-    "password": "",
+    "password": "" 
   }
-
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  
+  constructor(private formBuilder: FormBuilder) { }
+  
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    })
-  }
-
-
-  submitLogin() {
-    this.submitted = true;
-    if (this.loginForm.invalid) {
-      return
-    } else {
-      this.userLogin.email = this.loginForm.value.email
-      this.userLogin.password = this.loginForm.value.password
-      if (this.userLogin.email == "admin@gmail.com" && this.userLogin.password == "123456789") {
-        alert("เข้าสู่ระบบสำเร็จ")
-      } else {
-        alert("เข้าระบบไม่สำเร็จ")
+  this.loginFrom = this.formBuilder.group({
+     email:['', [Validators.required, Validators.email]],
+     password:['', [Validators.required, Validators.minLength(6)]],
+   })
+ }
+//ปุ่มล็อคอิน
+ submitLogin() {
+  this.submitted = true;
+  //ถ้าฟอร์มไม่ถูกต้อง (invalid)
+  if (this.loginFrom.invalid) { 
+    return
+  }else{
+    this.userLogin.email = this.loginFrom.value.email
+    this.userLogin.password = this.loginFrom.value.password
+    if(this.userLogin.email == "admin@gmail.com" 
+      && this.userLogin.password == "123456789"){
+        alert("Login Successful")
+  
+    }else{
+      alert("Login Failed")
       }
     }
   }
-
-
-resetForm(){
+//ปุ่ม reset From
+resetFrom(){
   this.submitted = false;
-  this.loginForm.reset();
-
+  this.loginFrom.reset();
 }
 }
